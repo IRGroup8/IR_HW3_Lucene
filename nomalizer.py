9aada2ffcfb189192e8901586dfda445f8d8c296
MohammadrezaPourreza <m1378.prz@gmail.com>
2020-10-15 10:53:20 +0330

    mamad

--- nomalizer.py
+++ nomalizer.py
@@ -4,6 +4,11 @@ file = open("group8Indexing.txt", "r")
 text = file.read()
 docs = text.split(".I")
 for doc in docs :
-    print(doc)
-    print("----------")
+    document = doc.split(".W")
+    #print(document)
+    if(len(document)>1):
+        docNum = document[0]
+        print(docNum)
+        docText = document[1]
+        print(docText)
 

6b73054366ca32102ef632e88da015ce6522c226
rezaBarzgar <r.barzgar98@gmail.com>
2020-10-15 10:53:29 +0330

    normilizing

--- nomalizer.py
+++ nomalizer.py
@@ -3,7 +3,11 @@ from nltk import *
 file = open("group8Indexing.txt", "r")
 text = file.read()
 docs = text.split(".I")
-for doc in docs :
-    print(doc)
-    print("----------")
+small_docs = []
+for doc in docs:
+    small_docs.append(doc.split(".W"))
 
+for doc in small_docs:
+    if (len(doc) > 1):
+        print(doc[1])
+        print("----------")
